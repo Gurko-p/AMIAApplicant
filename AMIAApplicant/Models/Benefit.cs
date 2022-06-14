cf485fe100ad3ccf681846ed8a255179c4d5ecf8
Pavel <gurko-p@mail.ru>
2022-06-14 17:08:02 +0300

    add Benefit class

---
+++ AMIAApplicant/Models/Benefit.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AMIAApplicant.Models
+{
+    public class Benefit
+    {
+        public int Id { get; set; }
+        public string BenefitFullName { get; set; }
+        public string BenefitShortName { get; set; }
+        public double PercentageOfBenefit { get; set; } // Процент от выделенного количества мест по льготе
+    }
+}
